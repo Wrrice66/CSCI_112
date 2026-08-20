ef9e95521bec118ab8569ca1ff741e0de80df5ab
Willon <wrrice@mavs.coloradomesa.edu>
2026-08-20 15:46:13 -0600

    Started Lab 1

--- Labs/Lab_1/src/gameOfLife.cpp
+++ Labs/Lab_1/src/gameOfLife.cpp
@@ -1,46 +1,54 @@
-#pragma once
-#include <fstream>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-struct Cell
-{
-    int x, y;
-    int state;
-    int numLiveNeighbors = 0;
-};
+#include "../include/gameOfLife.hpp"
 
 /*
 Function to clear terminal depending on OS
 */
-void clearScreen();
+void clearScreen()
+{
+    // use "cls" in windows and "clear" command in Mac and Linux
+    #ifdef _WIN32
+        system("clS");
+    #else
+        system("clear");
+    #endif
+}
 
 /*
 Function to initialize all cells in the board.
 Takes in 2D array of pointers and size of array
 Should create structs and populate the array
 */
-void initCells(Cell* board[][10], int boardSize);
+void initCells(Cell* board[][10], int boardSize)
+{
+
+}
 
 /*
 Function to read the board from a file
 Prompt for the file to read inside of the function
 Structure of file should consist of 10 lines of 0 or 1 to indicate cell state
 */
-void readBoard(Cell* board[][10], int boardSize);
+void readBoard(Cell* board[][10], int boardSize) 
+{
+
+}
 
 /*
 Function to print out all cells to cout
 */
-void printCells(Cell* board[][10], int boardSize);
+void printCells(Cell* board[][10], int boardSize)
+{
+
+}
 
 /*
 Function to count the number of live neighbors for each cell.
 Must use the x, y position stored with each cell to determine which neighbors they have
 */
-void findNumNeighbors(Cell* board[][10], int boardSize, Cell* curCell);
+void findNumNeighbors(Cell* board[][10], int boardSize, Cell* curCell) 
+{
+
+}
 
 /*
 Function to update each cell's state based on number of neighbors
@@ -53,4 +61,7 @@ Any dead cell with exactly three live neighbors becomes a live cell, as if by re
 
 Return if you updated cells or not to break out of while loop from main.
 */
-bool updateCellState(Cell* board[][10], int boardSize);
+bool updateCellState(Cell* board[][10], int boardSize) 
+{
+    return false;
+}
