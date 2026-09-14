fb0eb502cb51f7544a7311d733eacd77305b41c8
Willon <wrrice@mavs.coloradomesa.edu>
2026-09-14 14:51:18 -0600

    Fixed many issues with lab 1, fixed according to feedback, still haven't fixed findNumNeighbors yet. Will do so later

--- Labs/Lab_1/src/gameOfLife.cpp
+++ Labs/Lab_1/src/gameOfLife.cpp
@@ -41,7 +41,7 @@ void readBoard(Cell* board[][10], int boardSize)
 {
     ifstream fin;
     string saveName;
-    char currentLine[10];
+    std::string currentLine;
     while(!fin.is_open())
     {
         std::cout << "Enter name of savefile: " << std::endl;
@@ -52,7 +52,7 @@ void readBoard(Cell* board[][10], int boardSize)
     }
     for (int x = 0; x < boardSize; x++)
     {
-        fin.getline(currentLine, 10);
+        getline(fin, currentLine);
         for (int y = 0; y < 10; y++)
         {
             board[x][y]->state = (currentLine[y] - '0');
@@ -70,7 +70,7 @@ void printCells(Cell* board[][10], int boardSize)
     {
         for (int k = 0; k < 10; k++)
         {
-            std::cout << board[k][i]->state;
+            std::cout << board[i][k]->state;
         }
         std::cout << std::endl;
     }
@@ -82,6 +82,83 @@ Must use the x, y position stored with each cell to determine which neighbors th
 */
 void findNumNeighbors(Cell* board[][10], int boardSize, Cell* curCell) 
 {
+    if (curCell->x > 0 && curCell->y > 0)
+    {
+
+    }
+    else if (curCell->x > 0 && curCell->y == 0)
+    {
+
+    }
+    else if (curCell->x == 0 && curCell->y > 0)
+    {
+
+    }
+    else
+    {
+
+    }
+}
+
+/*
+Function to update each cell's state based on number of neighbors
+Must use following rules:
+
+Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by overpopulation.
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+Return if you updated cells or not to break out of while loop from main.
+*/
+bool updateCellState(Cell* board[][10], int boardSize) 
+{
+    bool cellChanged = false;
+    Cell* currCell;
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            currCell = board[i][j];
+            findNumNeighbors(board, boardSize, currCell);
+            if (currCell->state == 1)
+            {
+                switch (currCell->numLiveNeighbors)
+                {
+                    case 1:
+                        currCell->state = 0;
+                    case 2:
+                        continue;
+                    case 3:
+                        continue;
+                    case 4:
+                        currCell->state = 0;
+                    case 5:
+                        currCell->state = 0;
+                    case 6:
+                        currCell->state = 0;
+                    case 7:
+                        currCell->state = 0;
+                    case 8:
+                        currCell->state = 0;
+                }
+            }
+            else
+            {
+                if (currCell->numLiveNeighbors == 3)
+                    currCell->state = 1;
+                else
+                    continue;
+            }
+        }
+    }
+    return cellChanged;
+}
+
+
+// Old code for findNumNeighbors
+
+/*
     for (int y = 0; y < boardSize; y++)
     {
         for (int x = 0; x < 10; x++)
@@ -141,26 +218,12 @@ void findNumNeighbors(Cell* board[][10], int boardSize, Cell* curCell)
             }
         }
     }
-}
+*/
+
+
+// Old code for updateCellState
 
 /*
-Function to update each cell's state based on number of neighbors
-Must use following rules:
-
-Any live cell with fewer than two live neighbors dies, as if by underpopulation.
-Any live cell with two or three live neighbors lives on to the next generation.
-Any live cell with more than three live neighbors dies, as if by overpopulation.
-Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-
-Return if you updated cells or not to break out of while loop from main.
-*/
-bool updateCellState(Cell* board[][10], int boardSize) 
-{
-    bool cellChanged = false;
-    for (int i = 0; i < boardSize; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
             if (board[i][j]->state == 1)
             {
                 if (board[i][j]->numLiveNeighbors < 2)
@@ -168,7 +231,7 @@ bool updateCellState(Cell* board[][10], int boardSize)
                     board[i][j]->state = 0;
                     cellChanged = true;
                 }
-                else if (board[i][j]->numLiveNeighbors == 2||3)
+                else if (board[i][j]->numLiveNeighbors == 2||board[i][j]->numLiveNeighbors == 3)
                     continue;
                 else if (board[i][j]->numLiveNeighbors > 3)
                 {
@@ -186,7 +249,4 @@ bool updateCellState(Cell* board[][10], int boardSize)
                 else
                     continue;
             }
-        }
-    }
-    return cellChanged;
-}
+*/
